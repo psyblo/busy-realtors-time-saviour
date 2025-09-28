d96e23afca8e6538c28822eab82b97106c8d52a5
psyblo <sarpa.pablo96@gmail.com>
2025-09-28 15:25:37 +0200

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-
-import type { Config } from 'tailwindcss'
-
-export default {
-  content: ['./index.html', './src/**/*.{ts,tsx,jsx,js}'],
-  theme: { extend: {} },
-  plugins: [],
-} satisfies Config
